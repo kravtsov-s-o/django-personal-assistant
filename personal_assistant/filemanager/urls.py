915c385c3f635e4021256a345e26d21b4e09eeee
Alya <alya.v.kravchenko@gmail.com>
2024-02-24 13:54:28 +0200

    download and delete files

--- personal_assistant/filemanager/urls.py
+++ personal_assistant/filemanager/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('add_file/', views.upload_file, name='add_file'),
     path('add_success/', views.upload_success, name='add_success'),
     path('uploaded_files/', views.uploaded_files, name='uploaded_files'),
-    path('download/<str:file_name>/', views.download_file, name='download_file'),
+    path('download/<int:file_id>/', views.download_file, name='download_file'),
+    path('delete_file/<int:pk>/', views.delete_file, name='delete_file'),
     #path('create_category/', views.create_category, name='create_category'),
     
 ] 
--- personal_assistant/filemanager/views.py
+++ personal_assistant/filemanager/views.py
@@ -5,6 +5,12 @@ from .forms import FileUploadForm #CategoryForm
 from .models import File
 from django.http import HttpResponse
 from django.core.files.storage import default_storage
+from django.views.decorators.http import require_POST
+from django.shortcuts import get_object_or_404
+import boto3
+from botocore.exceptions import ClientError
+from personal_assistant.settings import AWS_STORAGE_BUCKET_NAME as bucket_name
+
 #from .models import Category
 
 
@@ -36,20 +42,39 @@ def upload_success(request):
     return render(request, 'filemanager/add_success.html', {'files': files})
 
 
-@login_required(login_url='/signin/')
 def uploaded_files(request):
     files = File.objects.filter(user=request.user)
-    file_names = [file.file.name for file in files]
-    return render(request, 'filemanager/uploaded_files.html', {'file_names': file_names})
+    return render(request, 'filemanager/uploaded_files.html', {'files': files})
 
 
-@login_required(login_url='/signin/')
-def download_file(request, file_name):
+def download_file(request, file_id):
+    file_instance = get_object_or_404(File, pk=file_id)
+
     try:
         # Открываем файл для чтения
-        with default_storage.open(file_name) as f:
+        with default_storage.open(file_instance.file.name) as f:
             response = HttpResponse(f.read(), content_type='application/octet-stream')
-            response['Content-Disposition'] = f'attachment; filename="{file_name}"'
+            response['Content-Disposition'] = f'attachment; filename="{file_instance.file.name}"'
             return response
     except FileNotFoundError:
         return HttpResponse("File not found", status=404)
+    
+def delete_file(request, pk):
+    file_instance = get_object_or_404(File, pk=pk)
+
+    # Отримання імені файлу для видалення з бази даних
+    file_name = file_instance.file.name
+
+    # Видалення об'єкта з бази даних
+    file_instance.delete()
+
+    # Видалення файлу з Amazon S3
+    if file_name:
+        s3_client = boto3.client('s3')
+    try:
+        s3_client.delete_object(Bucket=bucket_name, Key=file_name)
+    except ClientError as e:
+        print(f'Failed to delete file from S3: {e}')
+
+    return redirect('filemanager:uploaded_files')
+
